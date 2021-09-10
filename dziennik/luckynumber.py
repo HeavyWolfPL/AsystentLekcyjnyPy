434f58d5568d81477c8ffdd6634e5adc3694c481
Wafel <heavywolfpl@gmail.com>
2021-09-10 18:42:35 +0200

    finally working

--- dziennik/luckynumber.py
+++ dziennik/luckynumber.py
@@ -1,6 +1,5 @@
-import asyncio
-import nest_asyncio
 import json
+import re
 from vulcan import Vulcan
 from vulcan import Account
 from vulcan import Keystore
@@ -16,24 +15,24 @@ with open("config.json", "r") as config:
         dziennikToken = data["dziennikToken"]
         dziennikSymbol = data["dziennikSymbol"]
         dziennikPin = data["dziennikPIN"]
-        
-
-
-if not dziennik_enabled:
-    print("Dziennik is disabled, this function should not run.")
 
 async def get_luckynumber():
-    with open("dziennik-config.json", "w") as config: 
-        dziennikKeystore = Keystore.create(device_model="Python Vulcan API")
-        #json.dump(dziennikKeystore.as_dict, config)
-        config.write(dziennikKeystore.as_json)
-    with open("acc-config.txt", "w") as config2: 
-        dziennikAccount = Account.register(dziennikKeystore, dziennikToken, dziennikSymbol, dziennikPin)
-        #json.dump(dziennikAccount.as_json, config2)
-        config2.write(f'{dziennikAccount}')
-    nest_asyncio.apply()
-    asyncio.get_event_loop().run_until_complete(dziennikAccount)
+    
+    with open("key-config.json") as f:
+        # load from a JSON string
+        dziennikKeystore = Keystore.load(f.read())
+    with open("acc-config.json") as f:
+        # load from a JSON string
+        dziennikAccount = Account.load(f.read())
     dziennikClient = Vulcan(dziennikKeystore, dziennikAccount)
+    await dziennikClient.select_student()  # select the first available student
+    print(dziennikClient.student)  # print the selected student
+    students = await dziennikClient.get_students()
+    dziennikClient.student = students[1]  # select the second student   
     lucky_number = await dziennikClient.data.get_lucky_number()
     await dziennikClient.close()
+    print(f'{str(lucky_number)}')
+    number = re.search('number=(.+?)\)', str(lucky_number))
+    if number:
+        lucky_number = number.group(1)
     return lucky_number
